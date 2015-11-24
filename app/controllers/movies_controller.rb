f49eaced365da25465a29a28a5ba35a24fa6d15b
JustynaStebel <justyna.stebel@gmail.com>
2015-11-24 22:00:03 +0100

    Movies controller prepared

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,54 @@
 class MoviesController < ApplicationController
+  before_action :movie_find, only: [:show,:edit, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new(movie_params)
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :date, :cover, :description, :category_name, :category_id)
+  end
+
+  def movie_find
+    @movie = Movie.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:error] = "Movie doesn't exist!"
+    redirect_to movies_path
+  end
 end
