04709de1484754a2f03d0d20c0d087f8b59cb3dc
JustynaStebel <justyna.stebel@gmail.com>
2015-11-26 20:08:44 +0100

    more styling added

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :movie do
     title "MyString"
-year "MyString"
-cover "MyString"
-description "MyText"
-category "MyString"
+    year "MyString"
+    cover "MyString"
+    description "MyText"
+    category "MyString"
   end
 
 end
